2fab40deaeaa54c3d69aa91a9f8c644292519e8a
Pavel_Vatsek <pvatsek@gmail.com>
2022-12-01 12:50:55 +0300

    Task complete

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,7 +5,53 @@
 
 Примеры: 
 ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-["1234", "1567", "-2", "computer science"] -> ["2"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
 ["Russia", "Denmark", "Kazan"] -> []
 */
 
+string[] text = {"hello", "2", "world", ":-)"};
+
+WriteArray(text, "Исходный массив:");
+Console.WriteLine();
+
+int sizeNewArray = 0;
+
+for (int i = 0; i < text.Length; i++)
+{
+    string temp = text[i];
+
+    int size = temp.Length;
+
+    if (size <= 3)
+        sizeNewArray++;
+}
+
+string[] newText = new string[sizeNewArray];
+
+int j = 0;
+
+for (int i = 0; i < text.Length; i++)
+{
+    string temp = text[i];
+
+    int size = temp.Length;
+
+    if (size <= 3)
+        {
+            newText[j] = temp;
+            j++;
+        }
+}
+
+WriteArray(newText, "Получившийся массив:");
+
+
+void WriteArray(string[] array, string message)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
